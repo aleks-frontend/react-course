21aa42eb78134deb8d35eff071b901a7b111860f
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2019-05-15 17:57:21 +0200

    chore: got to states

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Header from './Header';
+import Inventory from './Inventory';
+import Order from './Order';
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.addFish = this.addFish.bind(this);
+        // Initial State
+        this.state = {
+            fishes: {},
+            order: {}
+        }
+    }
+
+    addFish(fish) {
+        // update state
+        const fishes = { ...this.state.fishes };
+        // add in our new fish
+        const timestamp = Date.now();
+        fishes[`fish-${timestamp}`] = fish;
+        // set state
+        this.setState({ fishes: fishes });
+    }
+
+    render() {
+        return (
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header tagline="Fresh Sea Food Market!" />
+                </div>
+                <Order />
+                <Inventory addFish={this.addFish} />
+            </div>
+        )
+    }
+}
+
+export default App;
